6459c0470dee9c32bb0f22f639c2ca271c5be29f
Jeffrey Goodsell <jgoodsell@thesummitgrp.com>
2025-01-13 19:08:28 -0600

    cleanup

--- encrypted_json_fields/encryption.py
+++ encrypted_json_fields/encryption.py
@@ -6,7 +6,6 @@ import base64
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes, hmac
 from cryptography.exceptions import InvalidSignature
 import os
